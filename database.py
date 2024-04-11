9facea579c3c713be138737f2956d60f96f395a3
Eccllair <bright.stellar.accretion@gmail.com>
2024-04-11 14:25:56 +0700

    переход на асинхронное выполнение запросов

--- database.py
+++ database.py
@@ -1,17 +1,22 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
-from config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_DB
+from config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_DB, POSTGRES_TEST_SERVER
 
-SQLALCHEMY_DATABASE_URL=f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:5444/{POSTGRES_DB}'
-SQLALCHEMY_TEST_DATABASE_URL=f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:5440/{POSTGRES_DB}'
+SQLALCHEMY_DATABASE_URL=f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:5444/{POSTGRES_DB}'
+SQLALCHEMY_TEST_DATABASE_URL=f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_TEST_SERVER}:5440/{POSTGRES_DB}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-test_engine = create_engine(SQLALCHEMY_TEST_DATABASE_URL)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+test_engine = create_async_engine(SQLALCHEMY_TEST_DATABASE_URL)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=True)
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+async_test_session_maker = sessionmaker(test_engine, class_=AsyncSession, expire_on_commit=True)
+async def get_async_test_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
