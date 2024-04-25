ca3ba82abd1af5cc2af8b66c3583510c1422872e
Eccllair <bright.stellar.accretion@gmail.com>
2024-04-25 16:16:57 +0700

    добавлены put и delete для таблицы users

--- routers.py
+++ routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, update, delete
 
 from models.user_model import User
 from database import get_async_session
@@ -16,12 +16,13 @@ async def get_users(session: AsyncSession = Depends(get_async_session)):
 async def create_user(name: str, session: AsyncSession = Depends(get_async_session)):
     await session.execute(insert(User).values(name=name))
     await session.commit()
-    pass
 
 @users_router.put("/{id}")
-async def change_user(session: AsyncSession = Depends(get_async_session)):
-    pass
+async def change_user(id: int, name: str, session: AsyncSession = Depends(get_async_session)):
+    await session.execute(update(User).where(User.id==id).values(name=name))
+    await session.commit()
 
 @users_router.delete("/{id}")
-async def delete_user(session: AsyncSession = Depends(get_async_session)):
-    pass
+async def delete_user(id: int, session: AsyncSession = Depends(get_async_session)):
+    await session.execute(delete(User).where(User.id==id))
+    await session.commit()
