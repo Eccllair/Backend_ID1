14d31053e0f3542b8c68b16fbd1ffb409b5de662
Eccllair <bright.stellar.accretion@gmail.com>
2024-04-28 16:05:00 +0700

    Добавление роутера чатов в приложение

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from routers import users_router
+from routers import users_router, chat_router
 
 app = FastAPI()
 
 app.include_router(
     router=users_router,
     prefix="/users"
+)
+
+app.include_router(
+    router=chat_router,
+    prefix="/chats"
 )
